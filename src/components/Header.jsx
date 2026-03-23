133c76ad5b5a91e6cabb00f90e54e30302298c0a
Herik25 <harshparmar87990@gmail.com>
2026-03-24 00:25:47 +0530

    feat: initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div className='grid grid-cols-[auto_1fr_auto_1fr_auto]'>
+            <div>left</div>
+            <div></div>
+            <div>center</div>
+            <div></div>
+            <div>right</div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
